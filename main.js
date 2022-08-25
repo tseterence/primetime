f2eb82c6757fb91f1c84752aa6c8c8bc521f0aa9
ttse23 <terence.tse23@gmail.com>
2022-08-24 18:03:50 -0700

    Fix countdown timer to continue while tab minimized

--- main.js
+++ main.js
@@ -7,13 +7,14 @@ function isPrime(num) {
 }
 
 // return random num from 1 to num, inclusive
-function getNum(num = 200) {
+function getNum(num) {
     return Math.floor(Math.random() * num + 1)
 }
 
 function startScreen() {
     hideScreens('screen')
     toggleScreen('start-screen', true)
+    t.stop()
 }
 
 function startGame() {
@@ -22,44 +23,106 @@ function startGame() {
 
     console.log('game started')
     document.querySelector('#current-score').innerHTML = '0'
-    document.querySelector('#number').innerHTML = `${getNum()}`
-
-    startTimer()
+    document.querySelector('#number').innerHTML = `${getNum(Number(document.getElementById('inputMaximum').value))}`
+    
+    t.start()
 }
+
+class Timer {
+    constructor() {
+        this.el = {
+            seconds: document.getElementById('seconds'),
+            tenthSeconds: document.getElementById('tenthSeconds'),
+        }
+
+        this.interval = null
+        this.remainingSeconds = 0
+
+
+
+        this.startTime = 0
+        this.endTime = 0
+    }
+
+    updateTimer() {
+        const seconds = Math.floor(this.remainingSeconds / 1000)
+        const tenthSeconds = Math.floor((this.remainingSeconds % 1000) / 100)
+
+        this.el.seconds.innerHTML = seconds.toString().padStart(2, "0")
+        this.el.tenthSeconds.innerHTML = tenthSeconds
+    }
+
+    start() {
+        this.startTime = new Date().getTime()
+        this.remainingSeconds = Number(document.getElementById('inputSeconds').value * 1000)
+        this.endTime = this.startTime + this.remainingSeconds
+        
+
+        if (this.remainingSeconds === 0) return;
+
+        this.interval = setInterval(() => {
+            this.remainingSeconds = this.endTime - Date.now()
+            this.updateTimer()
+            if (this.remainingSeconds <= 0) {
+                this.stop()
+                document.getElementById('reason').innerHTML = `Time's up!`
+                endGame()
+            }
+        }, 1);
+        
+        //
+        // this.remainingSeconds = Number(document.getElementById('inputSeconds').value) * 10
+
+        // if (this.remainingSeconds === 0) return;
+
+        // this.interval = setInterval(() => {
+        //     this.remainingSeconds--
+        //     this.updateTimer()
+      
+        //     if (this.remainingSeconds === 0) {
+        //         this.stop()
+        //         document.getElementById('reason').innerHTML = `Time's up!`
+        //         endGame()
+        //     }
+        // }, 100);
+    }
+
+    stop() {
+        clearInterval(this.interval)
+        this.interval = null
+    }
+}
+
+const t = new Timer()
+
 
 document.querySelector('#no').addEventListener('click', checkPrime)
 document.querySelector('#yes').addEventListener('click', checkPrime)
 document.querySelector('#home').addEventListener('click', startScreen)
-const countdownEl = document.getElementById('countdown')
-
-function startTimer() {
-    const startingSeconds = Number(document.getElementById('seconds').value)
-    let time = startingSeconds * 10
-
-    let t = setInterval(updateTimer, 100)
-    function updateTimer() {
-        if (time >= 0) {
-            const seconds = Math.floor(time / 10)
-            let deciseconds = time % 10
-            countdownEl.innerHTML = `${seconds}.${deciseconds}`
-            time--
-        }
-        if (time === 0) {
-            clearInterval(t)
-            endGame()
-        }
-    }
-}
 
 
 function checkPrime(e) {
     if ((e.target.id === 'no' && isPrime(Number(document.querySelector('#number').innerHTML))) || (e.target.id === 'yes' && !isPrime(Number(document.querySelector('#number').innerHTML)))) {
         // wrong choice - game over
+        t.stop()
         endGame()
+        if (e.target.id === 'no') {
+            document.getElementById('reason').innerHTML = `${document.querySelector('#number').innerHTML} is prime`
+        } else if (e.target.id === 'yes') {
+            let factors = getFactors(Number(document.querySelector('#number').innerHTML))
+            document.getElementById('reason').innerHTML = `${factors[0]} x ${factors[1]} = ${document.querySelector('#number').innerHTML}`
+        }
     } else {
         // continue game
         document.querySelector('#current-score').innerHTML++
-        document.querySelector('#number').innerHTML = `${getNum()}`
+        document.querySelector('#number').innerHTML = `${getNum(Number(document.getElementById('inputMaximum').value))}`
+    }
+}
+
+// get factors of num
+function getFactors(num) {
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return [i, num / i]
     }
 }
 
@@ -68,13 +131,15 @@ function endGame() {
     toggleScreen('end-screen', true)
 
     console.log('game ended')
-    // show user why selection was wrong, divisor vs prime
+
+    displayResult()
     checkHighScore()
 }
 
 function displayResult() {
     // if prime, say prime
     // else show first whole factor
+    document.getElementById('final-score').innerHTML = `${document.getElementById('current-score').innerHTML}`
 }
 
 function checkHighScore() {
@@ -87,16 +152,16 @@ function resetHighScore() {
     document.querySelector('#high-score').innerHTML = '0'
 }
 
-function showInfo() {
+function showAbout() {
     hideScreens('screen')
-    toggleScreen('info-screen', true)
-    // stop timer (reset?)
+    toggleScreen('about-screen', true)
+    t.stop()
 }
 
 function showSettings() {
     hideScreens('screen')
     toggleScreen('settings-screen', true)
-    // stop timer (reset?)
+    t.stop()
 }
 
 function hideScreens(cl) {
@@ -111,5 +176,43 @@ function toggleScreen(id, toggle) {
 }
 
 
+// only whole integers greater than 0 allowed in input
+document.querySelector('#subOne').addEventListener('click', () => {
+    if (document.getElementById('inputMaximum').value > 1) {
+        document.getElementById('inputMaximum').innerHTML = document.getElementById('inputMaximum').value--
+    }
+})
+document.querySelector('#addOne').addEventListener('click', () => document.getElementById('inputMaximum').innerHTML = document.getElementById('inputMaximum').value++)
+
+document.querySelector('#subSec').addEventListener('click', () => {
+    if (document.getElementById('inputSeconds').value > 1) {
+        document.getElementById('inputSeconds').innerHTML = document.getElementById('inputSeconds').value--
+    }
+})
+document.querySelector('#addSec').addEventListener('click', () => document.getElementById('inputSeconds').innerHTML = document.getElementById('inputSeconds').value++)
+
+
 
 // horizontal swiping ability for mobile?
+// not allow < 1 for max and time in settings window
+
+
+// function startTimer() {
+//     const countdownEl = document.getElementById('countdown')
+//     const startingSeconds = Number(document.getElementById('seconds').value)
+//     let time = startingSeconds * 10
+
+//     let t = setInterval(updateTimer, 100)
+//     function updateTimer() {
+//         if (time >= 0) {
+//             const seconds = Math.floor(time / 10)
+//             let deciseconds = time % 10
+//             countdownEl.innerHTML = `${seconds}.${deciseconds}`
+//             time--
+//         }
+//         if (time === 0) {
+//             // clearInterval(t)
+//             endGame()
+//         }
+//     }
+// }
